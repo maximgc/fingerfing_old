4e70f453a0aacac0388950993d2614cd14c7c5be
maximgc <maximkdev@gmail.com>
2014-07-10 10:45:02 +0400

    SensorData

---
+++ FingerFing/test/com/trifle/fingerfing/client/widget/EffectColorTest.java
@@ -0,0 +1,35 @@
+package com.trifle.fingerfing.client.widget;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.trifle.fingerfing.client.widget.effect.EColor;
+import com.trifle.fingerfing.client.widget.effect.IllegalEColor;
+
+public class EffectColorTest {
+
+	static private EColor ec; 
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ec = new EColor(10, 20, 30);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testToStringRgbFomat() {
+		assertEquals("rgb(10, 20, 30)", ec.toStringRgbFomat());
+	}
+	
+	@Test(expected = IllegalEColor.class)
+	public void testToStringRgbFomatException() {
+		new EColor(10, 20, 300);
+	}
+
+}
